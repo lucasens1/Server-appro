4d77cdc4452218336a5206180c585798c672aeda
lucasens1 <lucasensini1@gmail.com>
2024-09-25 19:14:55 +0200

    minor adjustement

--- Back-end/config/config.php
+++ Back-end/config/config.php
@@ -3,7 +3,7 @@
 // Configurazione del server
 return [
     'db_host' => 'localhost',
-    'db_port' => '8889',
+    'db_port' => '8889', // 3306 su Windows
     'db_name' => 'gestionale_1',
     'db_user' => 'root',
     'db_pass' => 'root'
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <>
       <h1 className="text-center p-2">Gestionale</h1>
-      <div className="container">
+      <div className="container ms_bg-1">
         <section>
           <MyHeader/>
         </section>
-        <section className="p-2">
+        <section className="p-2 fc-w">
           <Activities/>
         </section>
       </div>
